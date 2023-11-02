e70e73d32503df4ba99b7fd345701e921cb4dc25
Harith Albadry <harith.albadry@gmail.com>
2023-11-02 15:28:11 +0000

    added images for products

---
+++ frontend/src/data.js
@@ -0,0 +1,52 @@
+const data = {
+    products:[
+        {
+            name: "Nike Slim Shirt",
+            slug: "nike_slim_shirt",
+            category: "Shirts",
+            Image: "/images/p1.jpg",
+            price: 50,
+            countInStock: 10,
+            brand: "Nike",
+            rating: 4.5,
+            numReviews: 10,
+            description: "High quality shirt"
+        },
+        {
+            name: "Nike T shirt",
+            slug: "nike_T_shirt",
+            category: "Shirts",
+            Image: "/images/p2.jpg",
+            price: 25,
+            countInStock: 30,
+            brand: "Nike",
+            rating: 4.7,
+            numReviews: 4,
+            description: "High quality shirt"
+        },
+        {
+            name: "Addidas Slim Shirt",
+            slug: "Addidas_slim_shirt",
+            category: "Shirts",
+            Image: "/images/p3.jpg",
+            price: 60,
+            countInStock: 5,
+            brand: "Addidas",
+            rating: 4.0,
+            numReviews: 13,
+            description: "good quality shirt"
+        },
+        {
+            name: "Addidas joggers",
+            slug: "addidas_joggers",
+            category: "Pants",
+            Image: "/images/p4.jpg",
+            price: 40,
+            countInStock: 23,
+            brand: "Addidas",
+            rating: 4.3,
+            numReviews: 10,
+            description: "great pants"
+        }
+    ]
+}
