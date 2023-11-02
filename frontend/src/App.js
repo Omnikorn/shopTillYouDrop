0afd2f4c818a1e3d92c07859f41c4fd291a1f15d
Harith Albadry <harith.albadry@gmail.com>
2023-11-02 14:41:13 +0000

    added fron end

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div >
+      <header >
+        <a href='/' >STYD</a>
+      </header>
+      <main>
+        List products
+      </main>
+    </div>
+  );
+}
+
+export default App;
