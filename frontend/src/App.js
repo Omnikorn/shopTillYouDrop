e216d52cc9d40fddcbdccd9abf9be95d4fcb0426
Harith Albadry <harith.albadry@gmail.com>
2023-11-03 11:17:21 +0000

    added images and styling for featured products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,33 @@
+import data from "./data"
 
 function App() {
-  return (
-    <div >
-      <header >
-        <a href='/' >STYD</a>
-      </header>
-      <main>
-        List products
-      </main>
-    </div>
-  );
+	return (
+		<div>
+			<header>
+				<a href="/">STYD</a>
+			</header>
+			<main>
+				<h1>Featured Products</h1>
+				<div className="products">
+					{data.products.map((product) => (
+						<div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+							<img src={product.Image} alt={product.name} />
+
+              </a>
+              <div className="product-info">
+              <a href={`/product/${product.slug}`}>
+							<p><strong>{product.name}</strong></p>
+              </a>
+							<p>£{product.price}</p>
+              <button>Add to cart</button>
+              </div>
+						</div>
+					))}
+				</div>
+			</main>
+		</div>
+	)
 }
 
-export default App;
+export default App
